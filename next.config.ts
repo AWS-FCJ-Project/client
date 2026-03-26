64dfa52f595bc8ae6bff797996a35f385f7cd3f9
nguyen-thanh-1 <ntthanh14052005@gmail.com>
2026-03-26 21:30:53 +0700

    feat: Implement real-time camera object detection using an ONNX model and configure API proxy settings.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    "anagrammatically-nonderogative-bibi.ngrok-free.dev",
+    "*.ngrok-free.dev"
+  ],
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8000/:path*', // Proxy to Backend
+      },
+    ]
+  },
 };
 
 export default nextConfig;
