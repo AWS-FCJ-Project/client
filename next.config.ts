7a69640025824c5b29cada6144b46fd3c2238bf1
nguyen-thanh-1 <ntthanh14052005@gmail.com>
2026-03-29 15:17:43 +0700

    chore(config): remove unused ngrok proxy domains from allowed origins

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: [
-    "anagrammatically-nonderogative-bibi.ngrok-free.dev",
-    "*.ngrok-free.dev",
-    "*.ngrok-free.app",
-    "21f9-210-245-36-176.ngrok-free.app"
-  ],
   async rewrites() {
     return [
       {
